4cf5b516622a30a70989cdb9af54a46679515bac
Buford Eeds <bufordeeds8@gmail.com>
2019-12-08 17:07:41 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+
+
 // Write your code here!
+
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+document.body.appendChild(newHeader)
+
+document.querySelector('h1#victory');
+
+newHeader.innerHTML = "Buford is the champion";
